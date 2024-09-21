74923818203dac3d7704fe3cc13b90771532f4d0
Lu-el <lubov.elunina@mail.ru>
2024-09-21 13:56:22 +0400

    kitchen

--- script/modules/nav.js
+++ script/modules/nav.js
@@ -1,15 +1,15 @@
 export const navAction = () => {
   const menuBtn = document.querySelector('.kitchen');
-  const nav = document.querySelector('nav');
-  const lineOne = document.querySelector('nav .menu-btn .line--1');
-  const lineTwo = document.querySelector('nav .menu-btn .line--2');
-  const lineThree = document.querySelector('nav .menu-btn .line--3');
-  const link = document.querySelector('.nav-links');
+  const nav = document.querySelector('.kitchen__nav');
+  // const lineOne = document.querySelector('nav .menu-btn .line--1');
+  // const lineTwo = document.querySelector('nav .menu-btn .line--2');
+  // const lineThree = document.querySelector('nav .menu-btn .line--3');
+  const link = document.querySelector('.kitchen__links');
   menuBtn.addEventListener('click', () => {
-    nav.classList.toggle('nav-open');
+    nav.classList.toggle('kitchen__nav_open');
     // lineOne.classList.toggle('line-cross');
     // lineTwo.classList.toggle('line-fade-out');
     // lineThree.classList.toggle('line-cross');
-    link.classList.toggle('fade-in');
+    link.classList.toggle('kitchen__links_fade-in');
   })
 }
